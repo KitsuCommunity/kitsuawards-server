5f7d03798bdfd5ad417997cdda0b908e8271fa33
Gakamine <jolyantoine@pm.me>
2022-02-06 18:30:53 +0100

    Update nominee_type.rb

--- app/graphql/types/nominee_type.rb
+++ app/graphql/types/nominee_type.rb
@@ -14,15 +14,15 @@ module Types
             if Year.last.show_results
                 judges=Judge.all.map(&:user_id)
                 Vote.where(user_id: judges, nominee: object).count
-            else
-#                response = RestClient.get("https://kitsu.io/api/edge/users?filter[self]=true", {'Authorization': 'Bearer '+token})
-#                if defined? JSON.parse(response.body)['data'][0]['id']
-#                  userid=JSON.parse(response.body)['data'][0]['id']
-#                  if userid == '195642' or userid == '171273'
+            elsif defined? token
+                response = RestClient.get("https://kitsu.io/api/edge/users?filter[self]=true", {'Authorization': 'Bearer '+token})
+                if defined? JSON.parse(response.body)['data'][0]['id']
+                  userid=JSON.parse(response.body)['data'][0]['id']
+                  if userid == '195642' or userid == '171273'
                     judges=Judge.all.map(&:user_id)
                     Vote.where(user_id: judges, nominee: object).count
-#                  end
-#                end
+                  end
+                end
             end
         end
         field :users_results, Integer, null: true do
@@ -34,17 +34,15 @@ module Types
             judges=Judge.all.map(&:user_id)
             judges_votes=Vote.where(user_id: judges)
             Vote.where.not(id: judges_votes,user_id: blacklist).where(nominee: object).count
-          else
-#            response = RestClient.get("https://kitsu.io/api/edge/users?filter[self]=true", {'Authorization': 'Bearer '+token})
-#            if defined? JSON.parse(response.body)['data'][0]['id']
-#              userid=JSON.parse(response.body)['data'][0]['id']
-#              if userid == '195642' or userid == '171273'
-                blacklist=Blacklist.where(acc_too_recent: true).or(Blacklist.where(acc_not_enough_entries: true)).or(Blacklist.where(acc_non_verified_email: true)).or(Blacklist.where(acc_default_pfp: true)).map(&:user_id).uniq
+          elsif defined? token
+            response = RestClient.get("https://kitsu.io/api/edge/users?filter[self]=true", {'Authorization': 'Bearer '+token})
+            if defined? JSON.parse(response.body)['data'][0]['id']
+              userid=JSON.parse(response.body)['data'][0]['id']
+              if userid == '195642' or userid == '171273'
                 judges=Judge.all.map(&:user_id)
-                judges_votes=Vote.where(user_id: judges)
-                Vote.where.not(id: judges_votes,user_id: blacklist).where(nominee: object).count
-#              end
-#            end
+                Vote.where(user_id: judges, nominee: object).count
+              end
+            end
           end
         end
     end
